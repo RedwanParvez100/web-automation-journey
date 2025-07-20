98ef334eea4d980bfaa8e62895a9eafcb30eddf6
RedwanParvez100 <106326119+RedwanParvez100@users.noreply.github.com>
2025-07-20 17:57:00 +0600

    rename folder name

--- 01_Open_Websitepage_testTitle/src/test/java/example/DriverSetup.java
+++ 01_Open_Websitepage_testTitle/src/test/java/example/DriverSetup.java
@@ -2,10 +2,7 @@ package example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 public class DriverSetup {
 
