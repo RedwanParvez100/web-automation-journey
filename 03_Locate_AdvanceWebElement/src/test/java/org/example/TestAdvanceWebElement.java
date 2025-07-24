13e52ce0f1886b33637cdf61ebdbeeac3eb56336
RedwanParvez100 <106326119+RedwanParvez100@users.noreply.github.com>
2025-07-24 22:57:04 +0600

    test advance web element

---
+++ 03_Locate_AdvanceWebElement/src/test/java/org/example/TestAdvanceWebElement.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class TestAdvanceWebElement extends DriverSetup{
+
+    @Test
+    public void TestElement() throws InterruptedException {
+
+        browser.get("https://trytestingthis.netlify.app/");
+
+        /*
+        WebElement radiobutton1 = browser.findElement(By.cssSelector("input[value='radio1']"));
+        radiobutton1.click();
+        Thread.sleep(5000);
+
+        WebElement selectDropdown = browser.findElement(By.xpath("//select[@id='dropdown-class-example']"));
+        selectDropdown.click();
+        Thread.sleep(5000);
+         */
+
+        // Child locate
+        System.out.println(browser.findElement(By.xpath("//div /h1")).getText());
+        System.out.println(browser.findElement(By.cssSelector("div > h1")).getText());
+
+        // Multiple attribute
+        System.out.println(browser.findElement(By.xpath("//a[@class='button bar-item' and @href='/']")).getText());
+        System.out.println(browser.findElement(By.cssSelector("a[class='button bar-item'][href='/']")).getText());
+
+
+        // using SubString
+        System.out.println(browser.findElement(By.xpath("//button[starts-with(@onclick,\" window.open('http://google.com',\")]")).getText());
+        System.out.println(browser.findElement(By.cssSelector("button[onclick^=\" window.open('http://google.com',\"]")).getText());
+
+
+
+
+
+    }
+}
