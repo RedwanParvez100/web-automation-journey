d63189e6adef459968c2eaf310f72e406d31c736
RedwanParvez100 <106326119+RedwanParvez100@users.noreply.github.com>
2025-07-30 12:33:03 +0600

    added some advanced level xpath & css selector
    Classname, Tagname, Id, Table child locate

--- 03_Locate_AdvanceWebElement/src/test/java/org/example/TestAdvanceWebElement.java
+++ 03_Locate_AdvanceWebElement/src/test/java/org/example/TestAdvanceWebElement.java
@@ -9,8 +9,6 @@ public class TestAdvanceWebElement extends DriverSetup{
     @Test
     public void TestElement() throws InterruptedException {
 
-        browser.get("https://trytestingthis.netlify.app/");
-
         /*
         WebElement radiobutton1 = browser.findElement(By.cssSelector("input[value='radio1']"));
         radiobutton1.click();
@@ -20,6 +18,26 @@ public class TestAdvanceWebElement extends DriverSetup{
         selectDropdown.click();
         Thread.sleep(5000);
          */
+
+        //Classname
+        browser.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement text = browser.findElement(By.cssSelector(".ui-autocomplete-input"));
+        text.sendKeys("locate using cssSelector");
+        Thread.sleep(3000);
+
+        browser.get("https://trytestingthis.netlify.app/");
+        //Tagname
+        System.out.println(browser.findElement(By.xpath("//h1")).getText());
+        System.out.println(browser.findElement(By.cssSelector("h1")).getText());
+
+        //Id
+        WebElement firstName = browser.findElement(By.xpath("//*[@id='fname']"));
+        firstName.sendKeys("Redwan by xpath");
+        Thread.sleep(3000);
+        firstName.clear();
+        firstName = browser.findElement(By.cssSelector("#fname"));
+        firstName.sendKeys("Redwan by cssSelector");
+        Thread.sleep(3000);
 
         // Child locate
         System.out.println(browser.findElement(By.xpath("//div /h1")).getText());
@@ -36,6 +54,15 @@ public class TestAdvanceWebElement extends DriverSetup{
         // Content Method (match any part in the content)
         System.out.println(browser.findElement(By.xpath("//button[contains(@onclick,\"open('http://google.com',\")]")).getText());
         System.out.println(browser.findElement(By.cssSelector("button[onclick*=\"open('http://google.com',\"]")).getText());
+
+        // Table child locate
+        System.out.println(browser.findElement(By.xpath("//tr /th[1]")).getText());
+        System.out.println(browser.findElement(By.xpath("//tr /th[3]")).getText());
+        System.out.println(browser.findElement(By.xpath("//tr /th[last()]")).getText());
+
+        System.out.println(browser.findElement(By.cssSelector("tr > th:first-child")).getText());
+        System.out.println(browser.findElement(By.cssSelector("tr > th:nth-child(3)")).getText());
+        System.out.println(browser.findElement(By.cssSelector("tr > th:first-child")).getText());
 
 
 
